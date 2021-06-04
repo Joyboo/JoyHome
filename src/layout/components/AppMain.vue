054d74b2e3035b61a59ee1306cea535e066c2c5d
Joyboo <zhoubo5692@126.com>
2021-06-04 14:55:55 +0800

    全局返回顶部组件

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -5,39 +5,46 @@
         <router-view :key="key" />
       </keep-alive>
     </transition>
+
+    <el-tooltip placement="top" content="返回顶部">
+      <back-to-top :visibility-height="300" />
+    </el-tooltip>
   </section>
 </template>
 
 <script>
-export default {
-  name: 'AppMain',
-  computed: {
-    cachedViews() {
-      return this.$store.state.tagsView.cachedViews
-    },
-    key() {
-      return this.$route.path
+  import BackToTop from '@/components/BackToTop'
+
+  export default {
+    name: 'AppMain',
+    components: { BackToTop },
+    computed: {
+      cachedViews() {
+        return this.$store.state.tagsView.cachedViews
+      },
+      key() {
+        return this.$route.path
+      }
     }
   }
-}
 </script>
 
 <style lang="scss" scoped>
-.app-main {
-  /* 50= navbar  50  */
-  min-height: calc(100vh - 50px);
-  width: 100%;
-  position: relative;
-  overflow: hidden;
-}
-
-.fixed-header+.app-main {
-  padding-top: 50px;
-}
-
-.hasTagsView {
   .app-main {
-    /* 84 = navbar + tags-view = 50 + 34 */
+    /* 50= navbar  50  */
+    min-height: calc(100vh - 50px);
+    width: 100%;
+    position: relative;
+    overflow: hidden;
+  }
+
+  .fixed-header+.app-main {
+    padding-top: 50px;
+  }
+
+  .hasTagsView {
+    .app-main {
+      /* 84 = navbar + tags-view = 50 + 34 */
     min-height: calc(100vh - 84px);
   }
 
