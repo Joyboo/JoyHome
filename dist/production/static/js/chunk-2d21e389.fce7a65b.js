d854378c14487e826f62e4fb2030bab0d0524ef7
Joyboo <zhoubo5692@126.com>
2021-06-21 21:38:25 +0800

    build

--- dist/production/static/js/chunk-2d21e389.fce7a65b.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21e389"],{d546:function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("h1",[e._v("build")])},c=[],l={name:"build"},a=l,d=t("2877"),i=Object(d["a"])(a,u,c,!1,null,"dc513844",null);n["default"]=i.exports}}]);
