e48df63a749594d98a12676488f639f2cbc8781a
Joyboo <zhoubo5692@126.com>
2021-07-21 15:24:56 +0800

    错误日志 & 一键复制

--- src/views/cerror/index.vue
+++ src/views/cerror/index.vue
@@ -17,7 +17,36 @@
       </template>
     </layout-filter>
 
-    <table-index :data="tableData" :loading.sync="loading" :column="column" :border="false" />
+    <table-index :data="tableData" :loading.sync="loading" :column="column" :border="false">
+      <template>
+
+        <el-table-column min-width="380" align="left" label="错误信息" prop="msg">
+          <template slot-scope="scope">
+            <span>
+              <div class="msgBox">
+                <el-tag type="danger">Msg:&nbsp;&nbsp;{{ scope.row.msg }}</el-tag>
+              </div>
+              <div class="msgBox">
+                <el-tag type="warning">Tag:&nbsp;&nbsp;{{ scope.row.tag }}</el-tag>
+              </div>
+              <div class="msgBox">
+                <el-tag type="primary">Info:&nbsp;{{ scope.row.info }}</el-tag>
+              </div>
+              <div class="msgBox">
+                <el-tag type="success">Url:&nbsp;&nbsp;{{ scope.row.url }}</el-tag>
+              </div>
+            </span>
+          </template>
+        </el-table-column>
+
+        <el-table-column min-width="800" align="left" prop="content" label="堆栈跟踪">
+          <template slot-scope="scope">
+            <span class="textBox" @click="clip(regexText(scope.row.content), $event)" v-html="regexText(scope.row.content)" />
+          </template>
+        </el-table-column>
+
+      </template>
+    </table-index>
 
     <pagination :total="total" :query="query" @search="search" />
   </div>
@@ -28,8 +57,9 @@
 import Pagination from '@/components/Pagination'
 import LayoutFilter from '@/components/LayoutFilter'
 import TableIndex from '@/components/TableData'
-import { mapGetters } from 'vuex'
+import { mapGetters, mapState } from 'vuex'
 import { cerrorIndex } from '@/api/cerror'
+import clip from '@/utils/clipboard'
 
 export default {
   name: 'cerror',
@@ -54,43 +84,31 @@ export default {
         {
           key: 'itime',
           text: '时间',
-          sort: true
-          // fixed: 'left',
-          // width: '150',
+          sort: true,
+          width: '200'
         }, {
           key: 'username',
           align: 'left',
           text: '用户信息',
-          width: '150',
+          width: '180',
           template: (index, row) => {
             return '<b>UID</b>: ' + row.uid + '<br><b>用户名</b>: ' + row.realname + '<br><b>账号</b>:' + index
           }
-        }, {
-          key: 'msg',
-          text: '错误信息',
-          align: 'left',
-          width: '300',
-          template: (index, row) => {
-            let html = '<span style="padding: 5px;">'
-            html += '<br><b>Msg</b>:&nbsp;<span style="color: #F56C6C;background-color: #FFF5EE;">' + index + '</span>'
-            html += '<br><b>Tag</b>:&nbsp;<span style="color: #E6A23C;background-color: #FFF5EE;">' + row.tag + '</span>'
-            html += '<br><b>Info</b>:<span style="color: #909399;background-color: #FFF5EE;">' + row.info + '</span>'
-            html += '<br><b>url</b>:&nbsp;<span style="color: #2E8B57;background-color: #FFF5EE;">' + row.url + '</span>'
-            html += '<span>'
-            return html
-          }
-        }, {
-          key: 'content',
-          text: '堆栈跟踪',
-          width: '1000'
         }
       ]
     }
   },
   computed: {
-    ...mapGetters(['size'])
+    ...mapGetters(['size']),
+    ...mapState({
+      theme: state => state.settings.theme
+    })
   },
   methods: {
+    clip,
+    regexText: function(value) {
+      return value.replace(/\n/g, '<br />')
+    },
     search() {
       this.loading = true
       cerrorIndex(this.query)
@@ -114,4 +132,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+  .textBox {
+    cursor: pointer;
+  }
+
+  .msgBox {
+    line-height: 38px;
+  }
 </style>
