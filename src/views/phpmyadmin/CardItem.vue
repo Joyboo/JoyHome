140f603c3dd14553464c18fee31e633a58fdade7
Joyboo <zhoubo5692@126.com>
2021-09-13 17:25:20 +0800

    合并其他卡片

---
+++ src/views/phpmyadmin/CardItem.vue
@@ -0,0 +1,115 @@
+<template>
+  <el-card :body-style="{ padding: '0px', height: '150px' }">
+    <!-- svg -->
+    <svg-icon v-if="iconClass" :icon-class="iconClass" class-name="svgName" />
+    <!-- image -->
+    <div v-else-if="image" style="text-align: center">
+      <img :src="require('@/assets/image/' + image)" class="imageName" alt="">
+    </div>
+    <!-- name -->
+    <div v-else class="textName">{{ name }}</div>
+
+    <div style="padding: 14px;">
+      <span :class="[className, 'name']">{{ name }}</span>
+      <div class="bottom clearfix">
+        <span class="url">{{ url }}</span>
+
+        <el-row :gutter="20">
+          <el-col :span="6" :offset="18">
+            <el-link :href="fullUrl || url" :underline="false" target="_blank" class="tolink">
+              <el-button type="text" class="button">{{ $t('open') }}</el-button>
+            </el-link>
+          </el-col>
+        </el-row>
+
+      </div>
+    </div>
+
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'CardItem',
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    iconClass: {
+      type: String,
+      default: ''
+    },
+    image: {
+      type: String,
+      default: ''
+    },
+    className: {
+      type: String,
+      default: ''
+    },
+    url: {
+      type: String,
+      default: ''
+    },
+    fullUrl: {
+      type: [String],
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+  $height: 150px;
+
+  .name {
+    height: 15px;
+  }
+
+  .url {
+    font-size: 13px;
+    color: #999;
+    white-space: nowrap; // 强制不换行
+    max-height: 15px;
+  }
+
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+
+  .tolink {
+    padding: 0;
+    float: right;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+
+  .clearfix:after {
+    clear: both
+  }
+
+  .svgName {
+    width: 100%;
+    height: 100%;
+  }
+
+  .textName {
+    text-align: center;
+    height: $height;
+    line-height: $height;
+    font-size: 4rem;
+    font-weight: 600;
+  }
+
+  .imageName {
+    width: auto;
+    height: $height;
+  }
+</style>
--- src/views/phpmyadmin/index.vue
+++ src/views/phpmyadmin/index.vue
@@ -1,74 +1,54 @@
 <template>
-  <el-row :class="{pma: device == 'desktop', pmaMobile: device == 'mobile'}">
+  <el-row class="view-container">
     <!-- 有一个el-col-offset-1 -->
     <el-col
-      v-for="(o, index) in pma"
+      v-for="(item, index) in cards"
       :key="index"
-      :xs="{span: 22}"
-      :sm="{span: 22}"
-      :md="{span: 22}"
-      :lg="{span: 10}"
-      :xl="{span: 10}"
-      style="margin-top: 3rem;"
+      v-bind="colSpan"
+      style="margin-top: 2rem;"
       :offset="1"
     >
-      <el-card :body-style="{ padding: '0px' }">
-        <svg-icon style="width: 100%;height: 100%;" icon-class="phpmyadmin" />
-        <div style="padding: 14px;">
-          <span :class="o.class">{{ o.name }}</span>
-          <div class="bottom clearfix">
-            <span class="time">{{ o.url }}</span>
-            <el-link :href="o.url" :underline="false" target="_blank" class="tolink">
-              <el-button type="text" class="button">{{ $t('open') }}</el-button>
-            </el-link>
-          </div>
-        </div>
-      </el-card>
+      <card-item v-bind="item" />
     </el-col>
   </el-row>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import CardItem from '@/views/phpmyadmin/CardItem'
+import { isArray } from '@/utils/validate'
 
 export default {
   name: 'phpmyadminindex',
+  components: { CardItem },
+  data() {
+    return {
+      colSpan: {
+        xs: { span: 22 },
+        sm: { span: 22 },
+        md: { span: 11 },
+        lg: { span: 6 },
+        xl: { span: 6 }
+      },
+      card: [] // <CardItem>[]
+    }
+  },
   computed: {
     ...mapGetters(['config', 'device']),
     pma() {
-      return this.config.pma
+      const pma = this.config.pma
+      if (!pma || !isArray(pma)) {
+        return []
+      }
+      return pma
+    },
+    cards() {
+      const card = this.card
+      return this.pma.concat(card)
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .pma {
-    padding: 5rem;
-  }
-
-  .time {
-    font-size: 13px;
-    color: #999;
-  }
-
-  .bottom {
-    margin-top: 13px;
-    line-height: 12px;
-  }
-
-  .tolink {
-    padding: 0;
-    float: right;
-  }
-
-  .clearfix:before,
-  .clearfix:after {
-    display: table;
-    content: "";
-  }
-
-  .clearfix:after {
-    clear: both
-  }
 </style>
